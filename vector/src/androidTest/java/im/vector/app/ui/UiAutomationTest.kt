1ef5abe82f4d863753eb0e8a1f96e4a0a41773cd
Michael Kaye <1917473+michaelkaye@users.noreply.github.com>
2022-07-08 12:58:11 +0100

    Robot changes to demo automation

--- vector/src/androidTest/java/im/vector/app/ui/UiAutomationTest.kt
+++ vector/src/androidTest/java/im/vector/app/ui/UiAutomationTest.kt
@@ -26,7 +26,6 @@ import im.vector.app.R
 import im.vector.app.espresso.tools.ScreenshotFailureRule
 import im.vector.app.features.MainActivity
 import im.vector.app.getString
-import im.vector.app.ui.robot.AnalyticsRobot
 import im.vector.app.ui.robot.ElementRobot
 import im.vector.app.ui.robot.settings.labs.LabFeature
 import im.vector.app.ui.robot.withDeveloperMode
@@ -50,7 +49,7 @@ import java.util.concurrent.TimeUnit
  */
 @RunWith(AndroidJUnit4::class)
 @LargeTest
-class UiAllScreensSanityTest {
+class UiAutomationTest {
 
     @get:Rule
     val testRule = RuleChain
@@ -60,81 +59,21 @@ class UiAllScreensSanityTest {
 
     private val elementRobot = ElementRobot()
 
-    val client = OkHttpClient()
-
-    val JSON: MediaType = "application/json".toMediaType()
-
-    fun post(url: String, json: String): String {
-        val body: RequestBody = json.toRequestBody(JSON)
-        val request: Request = Request.Builder()
-                .url(url)
-                .post(body)
-                .build()
-        client.newCall(request).execute().use { response -> return response.body!!.string() }
-    }
-
-    fun poll(url: String): JSONObject {
-        val request: Request = Request.Builder()
-                .url(url)
-                .build()
-
-        client.newCall(request).execute().use { response -> return JSONObject(response.body!!.string()) }
-    }
-
     // Last passing:
     // 2020-11-09
     // 2020-12-16 After ViewBinding huge change
     // 2021-04-08 Testing 429 change
     @Test
-    fun allScreensTest() {
+    fun runThroughTest() {
         IdlingPolicies.setMasterPolicyTimeout(120, TimeUnit.SECONDS)
 
-        // keep the same UUID during each run; otherwise it should be entirely random.
-        val uuid = UUID.randomUUID().toString()
-        val posturl = "http://10.0.2.2:5000/client/" + uuid + "/respond"
-        val pollurl = "http://10.0.2.2:5000/client/" + uuid + "/poll"
-        while (true) {
-            var poll_response = poll(pollurl)
-
-            var action = poll_response.get("action")
-            if (action == "login") {
-                val data = poll_response.getJSONObject("data")
-                val user = data.getString("username")
-                val pass = data.getString("password")
-                elementRobot.login(user, pass)
-                post(posturl, "{\"response\": \"loggedin\"}")
-            }
-            if (action == "register") {
-                val data = poll_response.getJSONObject("data")
-                val user = data.getString("username")
-                val pass = data.getString("password")
-                elementRobot.register(user, pass)
-                // TODO register and navigate to root menu
-                post(posturl, "{\"response\": \"registered\"}")
-            }
-            if (action == "idle") {
-                sleep(5000)
-            }
-            // client will be told to start OR accept cross signing request
-            if (action == "start_crosssign") {
-                elementRobot.startVerification()
-                post(posturl, "{\"response\": \"started_crosssign\"}")
-            }
-            if (action == "accept_crosssign") {
-                elementRobot.acceptVerification()
-                post(posturl, "{\"response\": \"accepted_crosssign\"}")
-            }
-            // Both clients will be told to verify the cross sign
-            if (action == "verify_crosssign_emoji") {
-                elementRobot.completeVerification()
-                post(posturl, "{\"response\": \"verified_crosssign\"}")
-            }
-            // exit test and reset for next adventure
-            if (action == "exit") {
-                break
-            }
-            sleep(1000)
-        }
+        elementRobot.login("user_c3437849-9a2d-442b-91fa-d671440296de", "bubblebobblebabble")
+        //sleep(60000)
+        elementRobot.startVerification()
+        sleep(6000)
+        //elementRobot.acceptVerification()
+        elementRobot.completeVerification()
+        sleep(1000)
     }
 }
 //        elementRobot.onboarding {
